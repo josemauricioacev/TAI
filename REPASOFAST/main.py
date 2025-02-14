bf2d4226901ece944c05d5b8262ea3940cbcdb41
josemauricioacev <122044542@upq.edu.mx>
2025-02-14 15:26:04 -0600

    Practica No.4: Repaso FastAPI a. Obtener todas las tareas.

---
+++ REPASOFAST/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+tareas = [
+    {
+        "id": 1,
+        "titulo": "Estudiar para el examen",
+        "descripcion": "Repasar los apuntes de TAI",
+        "vencimiento": "14-02-2025",
+        "estado": "completada"
+    }
+]
+
+#ENDPOINTS
+
+#a. Obtener todas las tareas.
+@app.get("/tareas")
+def obtener_tareas():
+    return tareas
