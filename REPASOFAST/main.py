5238c83468e443cf8dccd751f3bde5f9764c0af2
josemauricioacev <122044542@upq.edu.mx>
2025-02-17 18:08:04 -0600

    Practica No.4: Repaso FastAPI - SWAGGER

--- REPASOFAST/main.py
+++ REPASOFAST/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI, HTTPException
 
-app = FastAPI()
+app = FastAPI(
+    title = "Mi primer API",
+    description = "Jose Armando Mauricio Acevedo",
+    version = "10.0.0.1"
+)
 
 tareas = [
     {
@@ -9,18 +13,32 @@ tareas = [
         "descripcion": "Repasar los apuntes de TAI",
         "vencimiento": "14-02-2025",
         "estado": "completada"
+    },
+    {
+        "id": 2,
+        "titulo": "Estudiar para el examen",
+        "descripcion": "Repasar los apuntes de TAI",
+        "vencimiento": "11-02-2025",
+        "estado": "completada"
+    },
+    {
+        "id": 3,
+        "titulo": "Estudiar para el examen",
+        "descripcion": "Repasar los apuntes de TAI",
+        "vencimiento": "14-01-2025",
+        "estado": "incompletada"
     }
 ]
 
 #ENDPOINTS
 
 #a. Obtener todas las tareas.
-@app.get("/tareas")
+@app.get("/tareas", tags = ["Obten todas las tareas"])
 def obtener_tareas():
     return tareas
 
 #b. Obtener una tarea específica por su ID.
-@app.get("/tareas/{id}")
+@app.get("/tareas/{id}", tags = ["Encuentra una tarea por ID"])
 def obtener_tarea(id: int):
     for tarea in tareas:
         if tarea["id"] == id:
@@ -28,7 +46,7 @@ def obtener_tarea(id: int):
     raise HTTPException(status_code=404, detail="Tarea no encontrada")
 
 #c. Crear una nueva tarea.
-@app.post("/tareas")
+@app.post("/tareas", tags = ["Crea tareas"])
 def crear_tarea(tarea: dict):
     for t in tareas:
         if t["id"] == tarea["id"]:
@@ -37,7 +55,7 @@ def crear_tarea(tarea: dict):
     return {"mensaje": "Tarea creada exitosamente", "tarea": tarea}
 
 #d. Actualizar una tarea existente.
-@app.put("/tareas/{id}")
+@app.put("/tareas/{id}", tags = ["Actualiza tus tareas"])
 def actualizar_tarea(id: int, tarea_actualizada: dict):
     for tarea in tareas:
         if tarea["id"] == id:
@@ -46,7 +64,7 @@ def actualizar_tarea(id: int, tarea_actualizada: dict):
     raise HTTPException(status_code=404, detail="Tarea no encontrada")
 
 #e. Eliminar una tarea.
-@app.delete("/tareas/{id}")
+@app.delete("/tareas/{id}", tags = ["Elimina tareas"])
 def eliminar_tarea(id: int):
     for tarea in tareas:
         if tarea["id"] == id:
