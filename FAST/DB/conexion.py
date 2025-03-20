0055ccc67a5145b3d38557ba23289b5c9ddf2547
josemauricioacev <122044542@upq.edu.mx>
2025-03-19 18:12:29 -0600

    SQLAlchemy - Primer Avance

---
+++ FAST/DB/conexion.py
@@ -0,0 +1,8 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+dbName = 'usuarios.sqlite'
+base_dir = os.path.dirname(os.path.realpath(__file__))
+dbURL = f'sqlite:///{os.path.join(base_dir, dbName)}'
